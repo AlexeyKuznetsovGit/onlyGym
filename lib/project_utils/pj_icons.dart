bb4db54b775ecd865fe0ae67087ddd8fa5990efc
kulshinroman <kulshin@eticon.ru>
2023-05-15 16:07:06 +0700

    add Icons

--- lib/project_utils/pj_icons.dart
+++ lib/project_utils/pj_icons.dart
@@ -1,9 +1,50 @@
 class PjIcons {
-  //EN
-  //Add your icons as shown below:
-  //RU
-  //Добавляйте свои иконки как показано ниже:
-  //static String get carIcon => 'assets/icons/carIcon.svg';
-  //static String get appleIcon => 'assets/icons/appleIcon.png';
+  static String get akkCross => 'assets/icons/akk-cross.svg';
+  static String get akkMinus => 'assets/icons/akk-minus.svg';
+  static String get akkOk => 'assets/icons/akk-ok.svg';
+  static String get analytics => 'assets/icons/analytics.svg';
+  static String get athletes => 'assets/icons/athletes.svg';
+  static String get back => 'assets/icons/back.svg';
+  static String get camera => 'assets/icons/camera.svg';
+  static String get chest => 'assets/icons/chest.svg';
+  static String get cross => 'assets/icons/cross.svg';
+  static String get diary => 'assets/icons/diary.svg';
+  static String get edit => 'assets/icons/edit.svg';
+  static String get end => 'assets/icons/end.svg';
+  static String get error => 'assets/icons/error.svg';
+  static String get exercises => 'assets/icons/exercises.svg';
+  static String get goal => 'assets/icons/goal.svg';
+  static String get hide => 'assets/icons/hide.svg';
+  static String get hips => 'assets/icons/hips.svg';
+  static String get leftForearm => 'assets/icons/left-forearm.svg';
+  static String get leftHip => 'assets/icons/left-hip.svg';
+  static String get leftShin => 'assets/icons/left-shin.svg';
+  static String get leftShoulder => 'assets/icons/left-shoulder.svg';
+  static String get myTraining => 'assets/icons/my-training.svg';
+  static String get neck => 'assets/icons/neck.svg';
+  static String get notmyTraining => 'assets/icons/notmy-training.svg';
+  static String get notificationOff => 'assets/icons/notification-off.svg';
+  static String get notificationOn => 'assets/icons/notification-on.svg';
+  static String get pause => 'assets/icons/pause.svg';
+  static String get photos => 'assets/icons/photos.svg';
+  static String get plus => 'assets/icons/plus.svg';
+  static String get plusSmall => 'assets/icons/plus-small.svg';
+  static String get profile => 'assets/icons/profile.svg';
+  static String get qr => 'assets/icons/qr.svg';
+  static String get quit => 'assets/icons/quit.svg';
+  static String get redo => 'assets/icons/redo.svg';
+  static String get rightForearm => 'assets/icons/right-forearm.svg';
+  static String get rightHip => 'assets/icons/right-hip.svg';
+  static String get rightShin => 'assets/icons/right-shin.svg';
+  static String get rightShoulder => 'assets/icons/right-shoulder.svg';
+  static String get search => 'assets/icons/search.svg';
+  static String get settings => 'assets/icons/settings.svg';
+  static String get shoulders => 'assets/icons/shoulders.svg';
+  static String get show => 'assets/icons/show.svg';
+  static String get smallCross => 'assets/icons/small-cross.svg';
+  static String get start => 'assets/icons/start.svg';
+  static String get trash => 'assets/icons/trash.svg';
+  static String get waist => 'assets/icons/waist.svg';
+
 }
   
