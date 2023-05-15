a51db03a3369d21d3e2f4ebc0ef5cb601bdabc51
kulshinroman <kulshin@eticon.ru>
2023-05-15 17:51:13 +0700

    add Icons

--- lib/project_widgets/pj_text.dart
+++ lib/project_widgets/pj_text.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+enum PjTextStyle{
+  h1,
+  title,
+  bold,
+  regular,
+  medium,
+  tiny,
+  downmenu
+}
+
+// @todo: Не тестировалось
 class PjText extends StatelessWidget {
   final String text;
   final Color? color;
-  final double? fontSize;
-  final FontWeight? fontWeight; 
+  final PjTextStyle style;
 
   const PjText(
-      {Key? key, required this.text, this.color, this.fontSize = 14, this.fontWeight}) : super(key : key);
+      {Key? key, required this.text, required this.style, this.color}) : super(key : key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,10 +26,45 @@ class PjText extends StatelessWidget {
       text,
       style: TextStyle(
         fontWeight: fontWeight,
+        fontSize: fontSize.w,
         color: color,
-        fontSize: fontSize!.w,
+        height: height,
       ),
     );
   }
+
+  FontWeight get fontWeight{
+    if (style == PjTextStyle.h1 || style == PjTextStyle.title || style == PjTextStyle.bold) return FontWeight.w700;
+    if (style == PjTextStyle.medium || style == PjTextStyle.tiny) return FontWeight.w500;
+    if (style == PjTextStyle.regular || style == PjTextStyle.downmenu) return FontWeight.w400;
+    return FontWeight.w400;
+  }
+
+  double get fontSize{
+    if(style == PjTextStyle.h1) return 28;
+    if(style == PjTextStyle.title) return 20;
+    if(style == PjTextStyle.bold || style == PjTextStyle.regular) return 16;
+    if(style == PjTextStyle.medium) return 14;
+    if(style == PjTextStyle.tiny) return 12;
+    if(style == PjTextStyle.downmenu) return 10;
+    return 14;
+  }
+
+  double get height{
+    if(style == PjTextStyle.h1) return 28 / 22;
+    if(style == PjTextStyle.title) return 20 / 22;
+    if(style == PjTextStyle.bold || style == PjTextStyle.regular) return 16 / 22;
+    if(style == PjTextStyle.medium) return 14 / 22;
+    if(style == PjTextStyle.tiny) return 12 / 20;
+    if(style == PjTextStyle.downmenu) return 10 / 12;
+    return 14;
+  }
+
+  String get fontFamily{
+    if(style == PjTextStyle.downmenu) return "SFPro";
+    return "PtRoot";
+  }
+
+
 }
   
